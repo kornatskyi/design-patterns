b44141aeb97f72c7f96ab9beaef4b58d86a0eb8c
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-26 08:19:18 -0700

    Interfaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
 public class Main {
+    public static TaxCalculator getCalculator() {
+        return new TaxCalculator2019();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+        TaxCalculator calculator = getCalculator();
+        calculator.calculateTax();
+
     }
 }
