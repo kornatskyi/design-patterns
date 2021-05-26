534a81100c9653e427a58a9f23ddaded20af9a06
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-26 08:31:55 -0700

    Encapsulation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,12 @@
 package com.company;
 
 public class Main {
-    public static TaxCalculator getCalculator() {
-        return new TaxCalculator2019();
-    }
+
 
     public static void main(String[] args) {
-
-        TaxCalculator calculator = getCalculator();
-        calculator.calculateTax();
-
+        var account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
     }
 }
